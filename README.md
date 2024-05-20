9d99b8d4962a045fad5ccdd741da93e99491ee57
btabuenca <bernardo.tabuenca@gmail.com>
2024-05-20 18:03:22 +0200

    Base version 2024

---
+++ README.md
@@ -0,0 +1,2 @@
+# Lists available sensors in mobile device
+version PAS 2024 for Android Java and Kotlin DSL build.gradle.kts
--- app/src/main/java/es/upm/btb/availablesensors/MainActivity.java
+++ app/src/main/java/es/upm/btb/availablesensors/MainActivity.java
@@ -15,59 +15,59 @@ import java.util.List;
 
 public class MainActivity extends Activity implements SensorEventListener {
 
-	private static final String TAG = "btb";
+    private static final String TAG = "btb";
 
-	private SensorManager sensorManager;
+    private SensorManager sensorManager;
 
-	ListView lvSensorList;
+    ListView lvSensorList;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-		lvSensorList  = (ListView) findViewById(R.id.lv);
+        lvSensorList  = (ListView) findViewById(R.id.lv);
 
-		// Read available sensors
-		sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-		List<Sensor> sensorsList = sensorManager.getSensorList(Sensor.TYPE_ALL);
+        // Read available sensors
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        List<Sensor> sensorsList = sensorManager.getSensorList(Sensor.TYPE_ALL);
 
-		SensorListBaseAdapter adapter =  new SensorListBaseAdapter(this, sensorsList);
-		lvSensorList.setAdapter(adapter);
-
-
-		Log.i(TAG, sensorsList.size() +" sensors found:");
-		int iCount = 0;
-		for (Sensor s : sensorsList) {
-			iCount++;
-			Log.i(TAG, iCount+". "+s.toString());
-		}
-		Log.i(TAG, iCount +" sensors listed.");
-
-	}
+        SensorListBaseAdapter adapter =  new SensorListBaseAdapter(this, sensorsList);
+        lvSensorList.setAdapter(adapter);
 
 
+        Log.i(TAG, sensorsList.size() +" sensors found:");
+        int iCount = 0;
+        for (Sensor s : sensorsList) {
+            iCount++;
+            Log.i(TAG, iCount+". "+s.toString());
+        }
+        Log.i(TAG, iCount +" sensors listed.");
 
-	protected void onResume() {
-		super.onResume();
-		//onResume() register the accelerometer to start listening to events
-		//sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-	}
+    }
 
-	protected void onPause() {
-		super.onPause();
-		//onPause() unregister the accelerometer to stop listening to events
-		//sensorManager.unregisterListener(this);
-	}
 
-	@Override
-	public void onAccuracyChanged(Sensor sensor, int accuracy) {
-		
-	}
 
-	@Override
-	public void onSensorChanged(SensorEvent event) {
+    protected void onResume() {
+        super.onResume();
+        //onResume() register the accelerometer to start listening to events
+        //sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
 
-	}
+    protected void onPause() {
+        super.onPause();
+        //onPause() unregister the accelerometer to stop listening to events
+        //sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
 
 }
